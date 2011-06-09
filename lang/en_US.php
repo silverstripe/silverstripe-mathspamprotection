8bb002688e718b263263b182d6436ee5289b1ba0
Marijn Kampf <advertising@exadium.com>
2011-06-09 11:41:44 +0100

    Ordered numbering in language files for easier translation Added Dutch translation

--- lang/en_US.php
+++ lang/en_US.php
@@ -2,26 +2,27 @@
 
 global $lang;
 
-$lang['en_US']['MathSpamProtection']['EIGHT'] = 'eight';
-$lang['en_US']['MathSpamProtection']['EIGHTEEN'] = 'eighteen';
-$lang['en_US']['MathSpamProtection']['ELEVEN'] = 'eleven';
-$lang['en_US']['MathSpamProtection']['FIFTEEN'] = 'fifteen';
-$lang['en_US']['MathSpamProtection']['FIVE'] = 'five';
-$lang['en_US']['MathSpamProtection']['FOUR'] = 'four';
-$lang['en_US']['MathSpamProtection']['FOURTEEN'] = 'fourteen';
-$lang['en_US']['MathSpamProtection']['NINE'] = 'nine';
-$lang['en_US']['MathSpamProtection']['ONE'] = 'one';
-$lang['en_US']['MathSpamProtection']['SEVEN'] = 'seven';
+$lang['en_US']['MathSpamProtection']['ZERO'] = 			'zero';
+$lang['en_US']['MathSpamProtection']['ONE'] = 			'one';
+$lang['en_US']['MathSpamProtection']['TWO'] = 			'two';
+$lang['en_US']['MathSpamProtection']['THREE'] = 		'three';
+$lang['en_US']['MathSpamProtection']['FOUR'] = 			'four';
+$lang['en_US']['MathSpamProtection']['FIVE'] = 			'five';
+$lang['en_US']['MathSpamProtection']['SIX'] = 			'six';
+$lang['en_US']['MathSpamProtection']['SEVEN'] = 		'seven';
+$lang['en_US']['MathSpamProtection']['EIGHT'] = 		'eight';
+$lang['en_US']['MathSpamProtection']['NINE'] = 			'nine';
+$lang['en_US']['MathSpamProtection']['TEN'] = 			'ten';
+$lang['en_US']['MathSpamProtection']['ELEVEN'] = 		'eleven';
+$lang['en_US']['MathSpamProtection']['TWELVE'] = 		'twelve';
+$lang['en_US']['MathSpamProtection']['THIRTEEN'] = 	'thirteen';
+$lang['en_US']['MathSpamProtection']['FOURTEEN'] = 	'fourteen';
+$lang['en_US']['MathSpamProtection']['FIFTEEN'] = 	'fifteen';
+$lang['en_US']['MathSpamProtection']['SIXTEEN'] = 	'sixteen';
 $lang['en_US']['MathSpamProtection']['SEVENTEEN'] = 'seventeen';
-$lang['en_US']['MathSpamProtection']['SIX'] = 'six';
-$lang['en_US']['MathSpamProtection']['SIXTEEN'] = 'sixteen';
-$lang['en_US']['MathSpamProtection']['TEN'] = 'ten';
-$lang['en_US']['MathSpamProtection']['THIRTEEN'] = 'thirteen';
-$lang['en_US']['MathSpamProtection']['THREE'] = 'three';
-$lang['en_US']['MathSpamProtection']['TWELVE'] = 'twelve';
-$lang['en_US']['MathSpamProtection']['TWO'] = 'two';
+$lang['en_US']['MathSpamProtection']['EIGHTEEN'] = 	'eighteen';
 $lang['en_US']['MathSpamProtection']['WHATIS'] = 'What is %s plus %s?';
-$lang['en_US']['MathSpamProtection']['ZERO'] = 'zero';
 $lang['en_US']['MathSpamProtectionField']['SPAMQUESTION'] = 'Spam protection question: %s';
+$lang['en_US']['MathSpamProtectionField']['INCORRECTSOLUTION'] = 'Incorrect solution to the spam protection question, please try again.';
 
 ?>
