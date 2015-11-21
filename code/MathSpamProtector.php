82a51add398019b687ddb0eaef5faacd785a4a8d
helpfulrobot <chris@silverstripe.com>
2015-11-21 19:27:13 +1300

    Converted to PSR-2

--- code/MathSpamProtector.php
+++ code/MathSpamProtector.php
@@ -4,20 +4,22 @@
  * @package mathspamprotection
  */
 
-class MathSpamProtector implements SpamProtector {
-	
-	/**
-	 * Returns the {@link MathSpamProtectorField} associated with this protector
-	 *
-	 * @return MathSpamProtectorField
-	 */
-	public function getFormField($name = null, $title = null, $value = null) {
-		return new MathSpamProtectorField($name, $title, $value);
-	}
+class MathSpamProtector implements SpamProtector
+{
+    /**
+     * Returns the {@link MathSpamProtectorField} associated with this protector
+     *
+     * @return MathSpamProtectorField
+     */
+    public function getFormField($name = null, $title = null, $value = null)
+    {
+        return new MathSpamProtectorField($name, $title, $value);
+    }
 
-	/**
-	 * Not used by MathSpamProtector
-	 */
-	public function setFieldMapping($fieldMapping) {}
-
+    /**
+     * Not used by MathSpamProtector
+     */
+    public function setFieldMapping($fieldMapping)
+    {
+    }
 }
